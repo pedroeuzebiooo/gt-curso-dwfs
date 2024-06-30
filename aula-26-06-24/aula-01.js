b332ae9e47359185576d933893e02c967dd7a13b
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-06-30 18:35:40 -0300

    adiciona aula 01 do período 26 de junho de 2024

---
+++ aula-26-06-24/aula-01.js
@@ -0,0 +1,8 @@
+const valorUm = 2;
+const valorDois = 4;
+
+if (valorDois > valorUm) {
+  document.write(`${valorDois} é maior que ${valorUm}`);
+} else {
+  document.write(`${valorDois} não é maior que ${valorUm}`);
+}
