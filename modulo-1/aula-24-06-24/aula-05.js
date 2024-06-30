e981f7fecc219a244c47ee3fe6b4a2b7f0ccf82f
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-06-30 16:54:09 -0300

    adiciona aula 05 do período 24 de junho de 2024

---
+++ modulo-1/aula-24-06-24/aula-05.js
@@ -0,0 +1,11 @@
+const primeiroValor = Number(prompt("Digite o primeiro valor: "));
+const segundoValor = Number(prompt("Digite o segundo valor: "));
+const somaDoPrimeiroValorComOSegundoValor = primeiroValor + segundoValor;
+alert(
+  "A soma de " +
+    primeiroValor +
+    " com " +
+    segundoValor +
+    " é igual a: " +
+    somaDoPrimeiroValorComOSegundoValor
+);
