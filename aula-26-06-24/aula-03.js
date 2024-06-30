d8306bfff2a0578899739312685a38038ae8ba69
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-06-30 18:36:21 -0300

    adiciona aula 03 do período de 26 de junho de 2024

---
+++ aula-26-06-24/aula-03.js
@@ -0,0 +1,13 @@
+const modo = "escuro";
+
+switch (modo) {
+  case "escuro":
+    console.log("modo escuro");
+    break;
+  case "claro":
+    console.log("modo claro");
+    break;
+  default:
+    console.log("modo não identificado");
+    break;
+}
